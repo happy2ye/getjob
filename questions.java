537abad3b6ae6aff7ba84d5fd92acfc1173f3475
YeahY <happy2ye@hotmail.com>
2019-08-08 14:00:46 +0800

    Add files via upload

---
+++ questions.java
@@ -0,0 +1,38 @@
+// my day one lets start with some easy problems
+// from leetcode Q 1108
+
+// Input: address = "1.1.1.1"
+// "1[.]1[.]1[.]1"
+
+
+public class solution1108{
+
+     public static void main(String []args){
+        
+        String ans = findIP('1.1.1.1');
+        
+        System.out.println(ans);
+     }
+     // read the string and once it find . replace with [.]
+     public String findIP(String address){
+         // I dont know how jova works for reading string lol
+         // feel bad
+         // I think for my level of programming just read solution
+         // I found charAt() would work for read the string
+         // give a try
+         String adrs = ""; 
+         for(int i = 0; i < address.length(); i++){
+             if(address.charAt(i) == '.'){
+                 adrs = adrs + "[.]"";
+             }else{
+                 adrs = adrs + address.charAt(i);
+             }
+         }
+         
+         return adrs;
+         
+     }
+}
+
+// if know charAt(), this really is a easy question.
+// good start
